a0b23d905df9ed12f9a70d7961b438f162963b84
naoaki114514 <naoaki.galaxy@gmail.com>
2020-07-23 15:20:40 +0900

    model modify

--- model.py
+++ model.py
@@ -17,13 +17,11 @@ class ResNetBlock(nn.Module):
         conv_block = []
         conv_block += [nn.ReflectionPad2d(1),
                        nn.Conv2d(dim, dim, kernel_size=3),
-                       #nn.InstanceNorm2d(dim),
                        nn.BatchNorm2d(dim),
                        nn.LeakyReLU(0.2),
                        nn.ReflectionPad2d(1),
                        nn.Conv2d(dim, dim, kernel_size=3),
                        nn.BatchNorm2d(dim)
-                       #nn.InstanceNorm2d(dim)
                        ]
         self.conv_block = nn.Sequential(*conv_block)
 
@@ -40,17 +38,14 @@ class Generator(nn.Module):
             nn.ReflectionPad2d(3),
 
             nn.Conv2d(3, 64, kernel_size=7),
-            #nn.InstanceNorm2d(64),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
 
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
-            #nn.InstanceNorm2d(128),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
 
             nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
-            #nn.InstanceNorm2d(256),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
 
@@ -65,12 +60,10 @@ class Generator(nn.Module):
             ResNetBlock(256),
 
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
-            #nn.InstanceNorm2d(128),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
 
             nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
-            #nn.InstanceNorm2d(64),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
 
